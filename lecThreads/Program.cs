4a0bdf204d269b153a465ed3b23bc0acf07a5157
Иван Иван <remillius_dies@outlook.com>
2020-02-15 12:19:09 +0300

    join test

--- lecThreads/Program.cs
+++ lecThreads/Program.cs
@@ -11,24 +11,26 @@ namespace lecThreads
     {
         static void Main(string[] args)
         {
-            Thread thread = new Thread(Do);
-            thread.Start("first thread id in Do method");
+            Thread thread = new Thread(() => Console.WriteLine(Do("first thread id in Do method")) );
+            thread.Start();
 
-            thread = new Thread(Do);
-            thread.Start("second thread id in Do method");
+            thread = new Thread(() => Console.WriteLine(Do("second thread id in Do method")));
+            thread.Start();
+
+            thread.Join();
             Do("Main thread id in Do method");
           //  Console.WriteLine($"Main thread id {Thread.CurrentThread.ManagedThreadId}");
             Console.ReadLine();
         }
-        private static void Do(object data)
+        private static int Do(object data)
         {
             string message = (string)data;
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine($"{message} {Thread.CurrentThread.ManagedThreadId} - {DateTime.Now}");
                 Thread.Sleep(100);
-
             }
+            return Thread.CurrentThread.ManagedThreadId;
         }
     }
 }
