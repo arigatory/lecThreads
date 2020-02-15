e88b422f93fddae23ff9a61f354fe0c8fba5eebf
Иван Иван <remillius_dies@outlook.com>
2020-02-15 13:36:27 +0300

    one core test

--- lecThreads/Program.cs
+++ lecThreads/Program.cs
@@ -11,26 +11,29 @@ namespace lecThreads
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
+            Thread.Sleep(5000);
             Thread thread1 = new Thread(() => Console.WriteLine(Do("first thread id in Do method")) );
             thread1.IsBackground = true;
+            thread1.Priority = ThreadPriority.BelowNormal;
             thread1.Start();
 
             Thread thread2 = new Thread(() => Console.WriteLine(Do("second thread id in Do method")));
             thread2.IsBackground = true;
+            thread2.Priority = ThreadPriority.Lowest;
             thread2.Start();
 
-            Thread.Sleep(2000);
+            //Thread.Sleep(2000);
             //Do("Main thread id in Do method");
-          Console.WriteLine($"Main thread id {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"Main thread id {Thread.CurrentThread.ManagedThreadId}");
+            Console.ReadLine();
             
         }
-        private static int Do(object data)
+        private static int Do(string message)
         {
-            string message = (string)data;
-            for (int i = 0; i < 10; i++)
+            while (true)
             {
                 Console.WriteLine($"{message} {Thread.CurrentThread.ManagedThreadId} - {DateTime.Now}");
-                Thread.Sleep(1000);
             }
             return Thread.CurrentThread.ManagedThreadId;
         }
